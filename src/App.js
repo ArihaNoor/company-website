20e8111e470728eed597010e259bc8f8e0ae89ce
Ariha Noor <arihanoor0@gmail.com>
2024-08-19 21:35:01 +0500

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Components/Navbar'
+import Hero from './Components/Hero'
+import Outline from './Components/Outline'
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Hero />
+    <Outline />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Outline.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Outline = () => {
+  return (
+    <div className='outline'>
+      <h1>Outline</h1>
+    </div>
+  )
+}
+
+export default Outline
